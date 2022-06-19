59c42182e75ce999af892b91cc5cd388eb4779e6
Matthew Coulter <mattcoul7@gmail.com>
2022-06-19 20:57:26 +1000

    Move Image Upload to edit menu

--- client/src/components/Website/PageComponents/Image/Photo1_EDIT.js
+++ client/src/components/Website/PageComponents/Image/Photo1_EDIT.js
@@ -7,23 +7,20 @@ import FileDAO from "../../../../DAOs/FileDAO";
 import EditComponent from "../EditComponent";
 
 export default class Photo1_Edit extends EditComponent {
+    initializeTabs() {
+        super.initializeTabs();
+        this.addTabContent("Content", () => <input className="form-control" id="formFileLg" type="file" onChange={(e) => {
+            var file = e.target.files[0];
+            if (!file) return;
+            FileDAO.insert(file)
+                .then((result) => {
+                    this.save({ src: result })
+                })
+        }} />)
+    }
     render() {
         return super.render(
-            <>
-                <img src={this.state.src} className="img-fluid" alt="Responsive image" />
-                {
-                    (() => {
-                        return this.state.focus ? <input className="form-control" id="formFileLg" type="file" onChange={(e) => {
-                            var file = e.target.files[0];
-                            if (!file) return;
-                            FileDAO.insert(file)
-                                .then((result) => {
-                                    this.save({ src: result })
-                                })
-                        }} /> : null
-                    })()
-                }
-            </>
+            <img src={this.state.src} className="img-fluid" alt="Responsive image" />
         );
     }
 }
